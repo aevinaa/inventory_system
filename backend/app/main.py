92af65c941de82529f3eb465f26884aa09db8329
Aditi <agrawaladiti247@gmail.com>
2026-05-18 16:24:18 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,7 +35,10 @@ app = FastAPI(
 # CORS: allows the React frontend to call this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origins=[
+    "http://localhost:5173",
+    "https://inventory-system-lyart.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
